6595e3727cb5fbce5820ca7a1de28641acdcda75
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-08-26 03:00:07 +0200

    add new filters;

---
+++ app/Filters/File/Copy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Forte\Api\Generator\Filters\File;
+
+use Zend\Filter\Exception\RuntimeException;
+use Zend\Filter\File\Rename;
+
+/**
+ * Class Copy. This class copies a source file to a given destination.
+ *
+ * @package Forte\Api\Generator\Filters\File
+ */
+class Copy extends Rename
+{
+    /**
+     * Copies the file $value to the new name set before.
+     * Returns the file $value, removing all but digit characters
+     *
+     * @param  string|array $value Full path of file to change or $_FILES data array
+     *
+     * @throws RuntimeException
+     *
+     * @return string|array The new filename which has been set
+     */
+    public function filter($value)
+    {
+        if (! is_scalar($value) && ! is_array($value)) {
+            return $value;
+        }
+
+        $file = $this->getNewName($value, true);
+        if (is_string($file)) {
+            return $file;
+        }
+
+        $result = copy($file['source'], $file['target']);
+
+        if ($result !== true) {
+            throw new RuntimeException(
+                sprintf(
+                    "File '%s' could not be renamed. " .
+                    "An error occurred while processing the file.",
+                    $value
+                )
+            );
+        }
+
+        return $file['target'];
+    }
+}
