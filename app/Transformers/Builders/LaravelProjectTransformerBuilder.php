760db6b49fbadcf0c78f1bc016d1cafaf2d5b279
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-08-27 11:08:11 +0200

    add custom method to configure the unzip action of a laravel project;

--- app/Transformers/Builders/LaravelProjectTransformerBuilder.php
+++ app/Transformers/Builders/LaravelProjectTransformerBuilder.php
@@ -2,6 +2,9 @@
 
 namespace Forte\Api\Generator\Transformers\Builders;
 
+use Forte\Api\Generator\Transformers\Transforms\Checks\ArrayCheckParameters;
+use Forte\Api\Generator\Transformers\Transforms\Checks\HasValidConfigEntries;
+
 /**
  * Class LaravelProjectTransformerBuilder. Class in charge of building a project transformer
  * with specific transformations for a Laravel project.
@@ -20,10 +23,39 @@ class LaravelProjectTransformerBuilder extends ProjectTransformerBuilder
     public function __construct(string $projectRootFolder, string $deploymentParentFolder)
     {
         parent::__construct($projectRootFolder, $deploymentParentFolder);
-
+//TODO I should be able to pass the zip file path as a parameter
+//TODO replace the init from zip with a new Transform instance that checks out the project from git
         $this
-            ->initFromZipFile('skeleton/forteframework-api-skeleton.zip')
+            ->addLaravelUnzipFileTransform('skeleton/forteframework-api-skeleton.zip')
             ->copyFileTo($this->getFilePathInProject('.env.example'), '.env')
-            ->copyFileTo($this->getFilePathInProject('.env.testing.example'), '.env.testing');
+            ->copyFileTo($this->getFilePathInProject('.env.testing.example'), '.env.testing')
+        ;
+    }
+
+    /**
+     * Adds a custom AbstractTransform subclass instance to unzip a
+     * Laravel zip into the configured class project folder.
+     *
+     * @param string $zipFilePath The file path of the zip containing a base Laravel project.
+     *
+     * @return LaravelProjectTransformerBuilder
+     */
+    public function addLaravelUnzipFileTransform(string $zipFilePath): self
+    {
+        $this->addTransform(
+            $this
+                ->getUnzipFileTransform($zipFilePath)
+                ->addAfterCheck(
+                    (new HasValidConfigEntries())
+                        ->open($this->getFilePathInProject('composer.json'))
+                        ->contentType(HasValidConfigEntries::CONTENT_TYPE_JSON)
+                        ->hasKeyWithNonEmptyValue("require.laravel/framework")
+                        ->hasKey("require.forteframework/api")
+                        ->hasKey("require.php")
+                        ->hasKeyWithValue("type", "project", ArrayCheckParameters::CHECK_EQUALS)
+                )
+        );
+
+        return $this;
     }
 }
