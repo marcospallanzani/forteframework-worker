7def1e79fcdb0760068f0fd0e45809c1794bb6fe
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-08-28 23:52:42 +0200

    add tests for FileTrait;

---
+++ tests/Unit/Helpers/FileTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit\Helpers;
+
+use Forte\Api\Generator\Exceptions\GeneratorException;
+use Forte\Api\Generator\Helpers\FileTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FileTest
+ *
+ * @package Tests\Unit\Helpers
+ */
+class FileTest extends TestCase
+{
+    /**
+     * Returns an anonymous class to test ClassAccessTrait.
+     *
+     * @return object
+     */
+    protected function getAnonymousTestClass()
+    {
+        return new class {
+            use FileTrait;
+        };
+    }
+
+    /**
+     * Data provider for all files tests.
+     *
+     * @return array
+     */
+    public function filesProvider(): array
+    {
+        return [
+            [__DIR__ . '/data/parsetest.ini', true, false, false],
+            [__DIR__ . '/data/parsetest.json', true, false, false],
+            [__DIR__ . '/data/parsetest.ini', true, true, false],
+            [__DIR__ . '/data/parsetest.json', true, true, false],
+            [__DIR__ . '/data/parsetest', false, false, false],
+            [__DIR__ . '/data/parsetest', false, true, true],
+        ];
+    }
+
+    /**
+     * Tests the FileTrait::checkFileExists() function.
+     *
+     * @dataProvider filesProvider
+     *
+     * @param string $filePath
+     * @param bool $expected
+     * @param bool $raiseError
+     * @param bool $exceptionExpected
+     */
+    public function testFileExists(string $filePath, bool $expected, bool $raiseError, bool $exceptionExpected): void
+    {
+        $class = $this->getAnonymousTestClass();
+        if ($exceptionExpected) {
+            $this->expectException(GeneratorException::class);
+        }
+        $this->assertEquals($expected, $class->checkFileExists($filePath, $raiseError));
+    }
+}
