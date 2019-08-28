bbb154518ec0c9beb2e68f68b7df7401cfcae9b0
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-08-28 19:07:41 +0200

    add new filter to modify the content of a given array; add unit tests;

---
+++ app/Filters/ModifyArray.php
@@ -0,0 +1,272 @@
+<?php
+
+namespace Forte\Api\Generator\Filters;
+
+use Forte\Api\Generator\Config\Reader;
+use Forte\Api\Generator\Exceptions\GeneratorException;
+use Forte\Api\Generator\Helpers\ClassConstantsTrait;
+use Forte\Api\Generator\Helpers\ThrowsErrors;
+
+/**
+ * Class ModifyArray.
+ *
+ * @package Forte\Api\Generator\Filters
+ */
+class ModifyArray
+{
+    use ClassConstantsTrait, ThrowsErrors;
+
+    /**
+     * Supported operations.
+     */
+    const MODIFY_ADD          = "modify_add";
+    const MODIFY_REMOVE_KEY   = "modify_remove_key";
+    const MODIFY_CHANGE_VALUE = "modify_change_value";
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var mixed|null
+     */
+    protected $value;
+
+    /**
+     * @var string
+     */
+    protected $operation;
+
+    /**
+     * ModifyArray constructor.
+     *
+     * @param string $key The array key to access (multi-level keys separated by '.').
+     * @param string $operation The operation to perform (one of the class constants
+     * starting with prefix 'MODIFY_').
+     * @param mixed  $value The value to set/change/remove.
+     */
+    public function __construct(string $key, string $operation, $value = null)
+    {
+        $this->key       = $key;
+        $this->operation = $operation;
+        $this->value     = $value;
+    }
+
+    /**
+     * Returns true if this ModifyArray instance is well configured:
+     * - key cannot be an empty string;
+     * - operation must equal to one of the class constants
+     *   starting with prefix 'MODIFY_';
+     *
+     * @return bool
+     *
+     * @throws GeneratorException
+     */
+    public function isValid(): bool
+    {
+        try {
+            $modifyConstants = self::getClassConstants('MODIFY_');
+        } catch (\ReflectionException $reflectionException) {
+            $this->throwGeneratorException(
+                "A general error occurred while retrieving the modifications list. Error message is: '%s'.",
+                $reflectionException->getMessage()
+            );
+        }
+
+        if (empty($this->key)) {
+            $this->throwGeneratorException("You need to specify the 'key' for the following check: '%s'.", $this);
+        }
+
+        // If no operation is specified OR an unsupported operation is given, then we throw an error.
+        if (!in_array($this->operation, $modifyConstants)) {
+            $this->throwGeneratorException(
+                "The operation '%s' is not supported. Impacted filter is: '%s'. Supported operations are: '%s'",
+                $this->operation,
+                $this,
+                implode(',', $modifyConstants)
+            );
+        }
+
+        return true;
+    }
+
+    /**
+     * Apply the configured change.
+     *
+     * @param array $array
+     *
+     * @return array
+     *
+     * @throws GeneratorException
+     */
+    public function filter(array $array): array
+    {
+        if ($this->isValid()) {
+            $this->applyChangeToArray($array, $this->key, $this->operation, $this->value);
+        }
+
+        return $array;
+    }
+
+    /**
+     * Returns the key.
+     *
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    /**
+     * Returns the value.
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Returns the operation.
+     *
+     * @return string
+     */
+    public function getOperation(): string
+    {
+        return $this->operation;
+    }
+
+    /**
+     * Applies the configured changes to the given array.
+     * This method supports multi-level arrays too.
+     *
+     * @param array  $array
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $modifiedValue
+     *
+     * @return array|mixed|null
+     */
+    public function applyChangeToArray(array &$array, string $key, string $operation, $modifiedValue)
+    {
+        $keysTree = explode(Reader::CONFIG_LEVEL_SEPARATOR, $key, 2);
+        $value = null;
+        if (count($keysTree) <= 2) {
+            // We check if a value for the current array key exists;
+            // If it does not exist, we throw an error.
+            $currentKey = $keysTree[0];
+            if (array_key_exists($currentKey, $array)) {
+                $value = $array[$currentKey];
+            }
+
+            // If a value for the current key was found, we check if we need
+            // to iterate again through the given elements tree;
+            if (count($keysTree) === 2) {
+                if(is_array($value)) {
+                    $value = $this->applyChangeToArray(
+                        $value,
+                        $keysTree[1],
+                        $operation,
+                        $modifiedValue
+                    );
+                    // We have to merge the modified sub-array with the parent array
+                    $array[$currentKey] = $value;
+                } else {
+                    // We have found a non-array element but we are not at the end of our keys tree
+                    if ($operation === self::MODIFY_ADD || $operation === self::MODIFY_CHANGE_VALUE) {
+                        // If key does not exist, we add the missing key
+                        // (no need to apply any changes for the remove action)
+                        $array[$currentKey] = [];
+                        $array[$currentKey] = $this->applyChangeToArray(
+                            $array[$currentKey],
+                            $keysTree[1],
+                            $operation,
+                            $modifiedValue
+                        );
+                    }
+                }
+            } else {
+                // We are at the end of our key tree: we have to modify the current key with the given value
+                $this->applyChangeByType(
+                    $array,
+                    $currentKey,
+                    $operation,
+                    $modifiedValue
+                );
+            }
+
+            // We set the modified array as the current value so that it will be added
+            // to the parent array in previous calls to this method
+            $value = $array;
+        }
+        return $value;
+    }
+
+    /**
+     * Applies the given operation to the given array for the given key and value.
+     *
+     * @param array  $array
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $value
+     *
+     * @return void
+     */
+    public function applyChangeByType(array &$array, string $key, string $operation, $value): void
+    {
+        switch($operation) {
+            case self::MODIFY_ADD:
+            case self::MODIFY_CHANGE_VALUE:
+                $array[$key] = $value;
+                break;
+            case self::MODIFY_REMOVE_KEY:
+                unset($array[$key]);
+                break;
+        }
+    }
+
+    /**
+     * Returns a human-readable description of this check operation.
+     *
+     * @return string
+     */
+    public function getOperationMessage(): string
+    {
+        switch($this->operation) {
+            case self::MODIFY_ADD:
+                return sprintf("Add value '%s' with key '%s'", $this->stringifyValue(), $this->key);
+            case self::MODIFY_CHANGE_VALUE:
+                return sprintf("Modify key '%s' and set it to '%s'", $this->key, $this->stringifyValue());
+            case self::MODIFY_REMOVE_KEY:
+                return sprintf("Remove key '%s'", $this->key);
+            default:
+                return "";
+        }
+    }
+
+    /**
+     * Returns a string version of the set value (it converts arrays to json).
+     *
+     * @return string
+     */
+    public function stringifyValue(): string
+    {
+        if (is_array($this->value)) {
+            return json_encode($this->value);
+        }
+        return (string) $this->value;
+    }
+
+    /**
+     * Returns a string representation of this ArrayCheckParameters instance.
+     *
+     * @return false|string
+     */
+    public function __toString()
+    {
+        return $this->getOperationMessage();
+    }
+}
---
+++ tests/Unit/Filters/ModifyArrayTest.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Tests\Unit\Filters;
+
+use Forte\Api\Generator\Exceptions\GeneratorException;
+use Forte\Api\Generator\Filters\ModifyArray;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ModifyArrayTest
+ *
+ * @package Tests\Unit\Filters
+ */
+class ModifyArrayTest extends TestCase
+{
+    /**
+     * Data provider for general modification tests.
+     *
+     * @return array
+     */
+    public function modificationsProvider(): array
+    {
+        return [
+            ['key1', ModifyArray::MODIFY_ADD, 'value1', "Add value 'value1' with key 'key1'"],
+            ['key1', ModifyArray::MODIFY_ADD, 'value1', "Add value 'value1' with key 'key1'"],
+            ['key1', ModifyArray::MODIFY_ADD, ['test-array' => 'array-value'], "Add value '{\"test-array\":\"array-value\"}' with key 'key1'"],
+            ['key1', ModifyArray::MODIFY_ADD, true, "Add value '1' with key 'key1'"],
+            ['key1', ModifyArray::MODIFY_ADD, null, "Add value '' with key 'key1'"],
+            ['key1', ModifyArray::MODIFY_CHANGE_VALUE, 'value1', "Modify key 'key1' and set it to 'value1'"],
+            ['key1', ModifyArray::MODIFY_CHANGE_VALUE, ['test-array' => 'array-value'], "Modify key 'key1' and set it to '{\"test-array\":\"array-value\"}'"],
+            ['key1', ModifyArray::MODIFY_CHANGE_VALUE, true, "Modify key 'key1' and set it to '1'"],
+            ['key1', ModifyArray::MODIFY_CHANGE_VALUE, null, "Modify key 'key1' and set it to ''"],
+            ['key1', ModifyArray::MODIFY_REMOVE_KEY, 'value1', "Remove key 'key1'"],
+            ['key1', "", 'value1', ""],
+        ];
+    }
+
+    /**
+     * Data provider to test changes on a 1-level array.
+     *
+     * @return array
+     */
+    public function changesProvider(): array
+    {
+        $simpleArray = ['test1' => 'value1', 'test2' => 'value2'];
+        $simpleModifiedArray = ['test1' => 'value1', 'test2' => 'value2', 'key1' => 'value1'];
+        return [
+            ['key1', ModifyArray::MODIFY_ADD, 'value1', $simpleArray, $simpleModifiedArray],
+            ['key1', ModifyArray::MODIFY_CHANGE_VALUE, 'value1', $simpleArray, $simpleModifiedArray],
+            ['key1', ModifyArray::MODIFY_REMOVE_KEY, 'value1', $simpleArray, $simpleArray],
+            ['test1', ModifyArray::MODIFY_REMOVE_KEY, 'value1', $simpleArray, ['test2' => 'value2']],
+        ];
+    }
+
+    /**
+     * Data provider to test changes on a N-level array. .
+     *
+     * @return array
+     */
+    public function complexChangesProvider(): array
+    {
+        $addArray = ['added' => 'value'];
+        $array = ['test1' => ['test3' => ['test4' => ['test5' => 'value5']]], 'test2' => 'value2'];
+        $modifiedArrayThreeLevels = ['test1' => ['test3' => ['test4' => ['key1' => $addArray, 'test5' => 'value5']]], 'test2' => 'value2'];
+        $modifiedArrayFourLevels = ['test1' => ['test3' => ['test4' => ['test6' => ['key1' => $addArray], 'test5' => 'value5']]], 'test2' => 'value2'];
+        return [
+            ['test1.test3.test4.key1', ModifyArray::MODIFY_ADD, $addArray, $array, $modifiedArrayThreeLevels],
+            ['test1.test3.test4.test6.key1', ModifyArray::MODIFY_ADD, $addArray, $array, $modifiedArrayFourLevels],
+            ['test1.test3.test4.key1', ModifyArray::MODIFY_CHANGE_VALUE, $addArray, $array, $modifiedArrayThreeLevels],
+            ['test1.test3.test4.test6.key1', ModifyArray::MODIFY_CHANGE_VALUE, $addArray, $array, $modifiedArrayFourLevels],
+            ['test2', ModifyArray::MODIFY_REMOVE_KEY, null, $array, ['test1' => ['test3' => ['test4' => ['test5' => 'value5']]]]],
+            ['test1.test3.test4.test5', ModifyArray::MODIFY_REMOVE_KEY, null, $array, ['test1' => ['test3' => ['test4' => []]], 'test2' => 'value2']],
+            ['test1.test3.test4', ModifyArray::MODIFY_REMOVE_KEY, null, $array, ['test1' => ['test3' => []], 'test2' => 'value2']],
+            ['test1.test3', ModifyArray::MODIFY_REMOVE_KEY, null, $array, ['test1' => [], 'test2' => 'value2']],
+            ['test1', ModifyArray::MODIFY_REMOVE_KEY, null, $array, ['test2' => 'value2']],
+            ['test-not-found', ModifyArray::MODIFY_REMOVE_KEY, null, $array, $array],
+        ];
+    }
+
+    /**
+     * Data provider for validation tests.
+     *
+     * @return array
+     */
+    public function validationProvider(): array
+    {
+        return [
+            ['key', ModifyArray::MODIFY_ADD, '', false, ['key' => '']],
+            ['', ModifyArray::MODIFY_ADD, '', true, null],
+            ['key1', 'wrong_action', '', true, null],
+            ['', '', '', true, null],
+        ];
+    }
+
+    /**
+     * Tests the operation message.
+     *
+     * @dataProvider modificationsProvider
+     *
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $value
+     * @param string $expected
+     */
+    public function testOperationMessage(string $key, string $operation, $value, string $expected): void
+    {
+        $modifyArray = new ModifyArray($key, $operation, $value);
+        $this->assertEquals($expected, $modifyArray->getOperationMessage());
+    }
+
+    /**
+     * Tests the applyChangeByType() function.
+     *
+     * @dataProvider changesProvider
+     *
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $value
+     * @param array  $array
+     * @param array  $expected
+     */
+    public function testApplyChangeByType(string $key, string $operation, $value, array $array, array $expected): void
+    {
+        $modifyArray = new ModifyArray($key, $operation, $value);
+        $modifyArray->applyChangeByType($array, $key, $operation, $value);
+        $this->assertEquals($expected, $array);
+    }
+
+    /**
+     * Tests the applyChangeToArray() function.
+     *
+     * @dataProvider complexChangesProvider
+     *
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $value
+     * @param array  $array
+     * @param array  $expected
+     */
+    public function testApplyChangeToArray(string $key, string $operation, $value, array $array, array $expected): void
+    {
+        $modifyArray = new ModifyArray($key, $operation, $value);
+        $modifyArray->applyChangeToArray($array, $key, $operation, $value);
+        $this->assertEquals($expected, $array);
+    }
+
+    /**
+     * Tests the isValid() function.
+     *
+     * @dataProvider validationProvider
+     *
+     * @param string $key
+     * @param string $operation
+     * @param mixed $value
+     * @param bool $expectGeneratorException
+     *
+     * @throws \Forte\Api\Generator\Exceptions\GeneratorException
+     */
+    public function testIsValid(string $key, string $operation, $value, bool $expectGeneratorException): void
+    {
+        $modifyArray = new ModifyArray($key, $operation, $value);
+
+        if ($expectGeneratorException) {
+            $this->expectException(GeneratorException::class);
+            $isValid = $modifyArray->isValid();
+            $this->assertFalse($isValid);
+        } else {
+            $isValid = $modifyArray->isValid();
+            $this->assertTrue($isValid);
+        }
+    }
+
+    /**
+     * Tests the filter() function.
+     *
+     * @dataProvider complexChangesProvider
+     *
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $value
+     * @param array  $array
+     * @param array  $expected
+     *
+     * @throws \Forte\Api\Generator\Exceptions\GeneratorException
+     */
+    public function testFilter(string $key, string $operation, $value, array $array, array $expected): void
+    {
+        $modifyArray = new ModifyArray($key, $operation, $value);
+        $modifiedArray = $modifyArray->filter($array);
+        $this->assertEquals($expected, $modifiedArray);
+    }
+
+    /**
+     * Tests the filter() function failures.
+     *
+     * @dataProvider validationProvider
+     *
+     * @param string $key
+     * @param string $operation
+     * @param mixed  $value
+     * @param bool   $expectGeneratorException
+     * @param mixed  $expected
+     *
+     * @throws \Forte\Api\Generator\Exceptions\GeneratorException
+     */
+    public function testFailFilter(string $key, string $operation, $value, bool $expectGeneratorException, $expected): void
+    {
+        $modifyArray = new ModifyArray($key, $operation, $value);
+        if ($expectGeneratorException) {
+            $this->expectException(GeneratorException::class);
+            $modifyArray->filter([]);
+        } else {
+            $modifiedArray = $modifyArray->filter([]);
+            $this->assertEquals($expected, $modifiedArray);
+        }
+    }
+}
