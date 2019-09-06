f8b44608b43a64ccabe514d13c93675635501609
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-09-06 13:40:22 +0200

    update EmptyTransform to support the new run and isValid signatures;

--- src/Actions/Transforms/EmptyTransform.php
+++ src/Actions/Transforms/EmptyTransform.php
@@ -3,6 +3,7 @@
 namespace Forte\Worker\Actions\Transforms;
 
 use Forte\Worker\Actions\AbstractAction;
+use Forte\Worker\Actions\ActionResult;
 
 /**
  * Class EmptyTransform. This class does not apply any transformation
@@ -13,28 +14,6 @@ use Forte\Worker\Actions\AbstractAction;
 class EmptyTransform extends AbstractAction
 {
     /**
-     * Whether this instance is in a valid state or not.
-     *
-     * @return bool True if this EmptyTransform instance
-     * was well configured; false otherwise.
-     */
-    public function isValid(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Apply the sub-class transformation action.
-     *
-     * @return bool True if the transform action implemented by this
-     * EmptyTransform instance was successfully applied; false otherwise.
-     */
-    protected function apply(): bool
-    {
-        return true;
-    }
-
-    /**
      * Returns a string representation of this EmptyTransform instance.
      *
      * @return string
@@ -42,5 +21,54 @@ class EmptyTransform extends AbstractAction
     public function stringify(): string
     {
         return "Empty transform";
+    }
+
+    /**
+     * Apply the sub-class transformation action.
+     *
+     * @param ActionResult $actionResult The action result object to register
+     * all failures and successful results.
+     *
+     * @return ActionResult The ActionResult instance with updated fields
+     * regarding failures and result content.
+     *
+     * @throws \Exception
+     */
+    protected function apply(ActionResult $actionResult): ActionResult
+    {
+        return $actionResult->setResult(true);
+    }
+
+    /**
+     * Validate the given action result. This method returns true if the
+     * given ActionResult instance has a result value that is considered
+     * as a positive case by this AbstractAction subclass instance.
+     *
+     * @param ActionResult $actionResult The ActionResult instance to be checked
+     * with the specific validation logic of the current AbstractAction subclass
+     * instance.
+     *
+     * @return bool True if the given ActionResult instance has a result value
+     * that is considered as a positive case by this AbstractAction subclass
+     * instance; false otherwise.
+     */
+    public function validateResult(ActionResult $actionResult): bool
+    {
+        // The ActionResult->result field should be set with a boolean
+        // representing the last execution of the apply method.
+        return (bool) $actionResult->getResult();
+    }
+
+    /**
+     * Validate this AbstractAction subclass instance using a validation logic
+     * specific to the current instance.
+     *
+     * @return bool True if no validation breaches were found; false otherwise.
+     *
+     * @throws \Exception If validation breaches were found.
+     */
+    protected function validateInstance(): bool
+    {
+        return true;
     }
 }
