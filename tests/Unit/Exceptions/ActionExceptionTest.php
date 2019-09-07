cb89f38c705b260b44fd44695ff3d5e8123afe79
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-09-07 12:49:05 +0200

    add test for ActionException methods;

---
+++ tests/Unit/Exceptions/ActionExceptionTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Exceptions;
+
+use Forte\Worker\Exceptions\ActionException;
+use Tests\Unit\BaseTest;
+
+/**
+ * Class ActionExceptionTest.
+ *
+ * @package Tests\Unit\Exceptions
+ */
+class ActionExceptionTest extends BaseTest
+{
+    /**
+     * Test for function ActionException::toArray().
+     */
+    public function testToArray(): void
+    {
+        $grandChildException = new ActionException($this->getAnonymousActionClass('grand child class'), 'Test grand child failure');
+        $childException = new ActionException($this->getAnonymousActionClass('child class'), 'Test child failure');
+        $childException->addChildFailure($grandChildException);
+        $actionException = new ActionException($this->getAnonymousActionClass('parent class'), 'Test primary failure');
+        $actionException->addChildFailure($childException);
+        $this->assertEquals(
+            [
+                'action' => 'parent class',
+                'error_message' => 'Test primary failure',
+                'children_failures' => [
+                    [
+                        'action' => 'child class',
+                        'error_message' => 'Test child failure',
+                        'children_failures' => [
+                            [
+                                'action' => 'grand child class',
+                                'error_message' => 'Test grand child failure',
+                                'children_failures' => [],
+                                'error_code' => 0
+                            ]
+                        ],
+                        'error_code' => 0
+                    ]
+                ],
+                'error_code' => 0
+            ],
+            $actionException->toArray()
+        );
+    }
+}
